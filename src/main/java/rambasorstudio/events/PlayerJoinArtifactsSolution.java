37d7b84d77e4400d957fc08a6263b089aad38c64
RambasorGameStudio <rambasorsl@gmail.com>
2022-09-02 18:35:42 +0200

    Version 1.0 || Error del mod artifacts solucionado
    ·Dependencias: Artifacts y SimpleTPA

--- src/main/java/rambasorstudio/events/PlayerJoinArtifactsSolution.java
+++ src/main/java/rambasorstudio/events/PlayerJoinArtifactsSolution.java
@@ -1,12 +1,18 @@
 package rambasorstudio.events;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 //Solucion al bug del menu del mod Artifacts para la 1.16.5 con ArcLight
+//Dependencias TPA y MultiverseCore
 public class PlayerJoinArtifactsSolution implements Listener {
     @EventHandler
-    public void artifacts (PlayerJoinEvent e){
-
+    public void artifacts(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        String name = player.getName();
+        e.getPlayer().performCommand("mvtp flatroom");
+        player.sendMessage("§9Servidor gestionado por Rambasor Game Studio");
+        e.getPlayer().performCommand("back");
     }
 }
